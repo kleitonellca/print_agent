1f19bff8404c6a04157cf52280f3209d9c4e200c
kleitonellca <kleitonellca@gmail.com>
2026-05-20 22:53:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.markdown("""
     <style>
         /* Força a aplicação a iniciar do topo absoluto, eliminando o corte da barra padrão */
         .stApp {
-            background-color: #F3F4F6 !important;
+            background-color: #F3F4F3 !important;
         }
         
         .block-container {
